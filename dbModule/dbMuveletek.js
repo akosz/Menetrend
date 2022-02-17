b5e32b2d6f8535f7ab56f9bf2b7e5a7b9330ade6
Szabó Ákos <szabo.akos2000@gmail.com>
2022-02-17 19:11:53 +0100

    first commit

---
+++ dbModule/dbMuveletek.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const config = require('../config/configDB.json');
+
+const connection = mysql.createConnection({
+    host: config.host,
+    port: config.port,
+    database: config.database,
+    user: config.user,
+    password: config.password
+});
+
+module.exports.vonatEsemenyLista = function (vId, callback) {
+    myQuery = `SELECT vNev, allId, ora, perc, jelleg FROM esemeny INNER JOIN vonat on vonat.vId=esemeny.vId WHERE vonat.vId=${vId}`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
+        }
+    })
+}
+
+module.exports.vonatLista = function (callback) {
+    myQuery = `SELECT vId, vNev FROM vonat`;
+    connection.query(myQuery, (err, result, fields) => {
+        if (err)
+            callback(err, null);
+        else {
+            callback(null, JSON.parse(JSON.stringify(result)));
+        }
+    })
+}
+
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const dbData = require("./dbModule/dbMuveletek.js");
+const path = require("path");
+//const { isBuffer } = require('util');
+const port = 4444;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+// const
+// = require(
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/ejs", (req, res) => {
+  dbData.vonatLista((err, data) => {
+    if (err) throw err;
+    res.render("index", { vonatok: data });
+  });
+});
+
+app.get("/vonat", (req, res) => {
+  if (req.query.vonatId) {
+    dbData.vonatEsemenyLista(req.query.vonatId, (err, data) => {
+      if (err) throw err;
+      res.set("Access-Control-Allow-Origin", "*");
+      res.send(data);
+    });
+  } else
+    dbData.vonatLista((err, data) => {
+      if (err) throw err;
+      res.set("Access-Control-Allow-Origin", "*");
+      res.send(data);
+    });
+});
+
+/*
+app.get('/vonatok', (req,res) =>{
+    
+});
+
+app.get('/allomasok', (req,res) =>{
+    
+});
+
+app.post('/ujvonat', (req,res) =>{
+    
+});
+
+app.post('/deleteVonat',  (req,res) =>{
+    
+});
+
+app.post('/idoModosit',  (req,res) =>{
+    
+});
+*/
+
+app.get("*", (req, res) => {
+  res.redirect("/ejs");
+});
+
+app.listen(port);
